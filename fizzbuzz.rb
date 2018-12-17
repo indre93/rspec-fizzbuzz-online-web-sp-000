07cc0467fec000f390bea2b3a6b46908430daf52
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-17 03:42:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,4 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 
-   return "Fizz"
-  elsif int % 5 == 0 
-  return "Buzz"
-  else % 3 and int % 5 == 0
-  return "FizzBuzz"
-  end
-end
 
